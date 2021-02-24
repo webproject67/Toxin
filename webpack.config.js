7ec3ea06e4583e88ccbef29f7c65755957415d82
webproject67 <webproject67@gmail.com>
2021-02-24 16:18:40 +0300

    старт

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9000
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    ...glob.sync('./src/pug/pages/**/*.pug')
+      .map(htmlFile => {
+        return new HtmlWebpackPlugin({
+          filename: `${path.basename(htmlFile, path.extname(htmlFile))}.html`,
+          template: htmlFile,
+          inject: true
+        });
+      }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src/assets/favicon/*.{png,svg}'),
+        to: path.resolve(__dirname, 'public/img/favicon/[name].[ext]')
+      },
+      {
+        from: path.resolve(__dirname, 'src/assets/favicon/*.{xml,webmanifest,ico}'),
+        to: path.resolve(__dirname, 'public/[name].[ext]')
+      }
+    ]),
+    new ImageminPlugin({ 
+      test: /\.svg$/i 
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(woff2|woff)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts'
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'img'
+        }
+      },
+      {
+        test: /\.(svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'img/svg'
+        }
+      },
+      {
+        test: /\.pug$/,
+        use: ['html-loader', 'pug-html-loader']
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ]
+  }
+}
