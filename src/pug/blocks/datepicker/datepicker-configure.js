1819d03e238f1e7a33882e4c81ff640adc6b31f1
webproject67 <webproject67@gmail.com>
2021-03-11 18:44:28 +0300

    рефакторинг cards

--- src/pug/blocks/datepicker/datepicker-configure.js
+++ src/pug/blocks/datepicker/datepicker-configure.js
@@ -1,35 +1,35 @@
-$(".form__block-inputs").each(function () {
-  const input1 = $(this).find("#input1");
-  const input2 = $(this).find("#input2");
+// $(".form__block-inputs").each(function () {
+//   const input1 = $(this).find("#input1");
+//   const input2 = $(this).find("#input2");
 
-  $(this).find(".datepicker-toggle").datepicker({
-    clearButton: true,
-    multipleDatesSeparator: ' - ',
-    range: true,
-    prevHtml: 'arrow_back',
-    nextHtml: 'arrow_forward',
-    navTitles: {
-      days: 'MM <i>yyyy</i>'
-  },
-    onSelect: (date) => {
-      const dates = date.split(" - ");
-      input1.val(dates[0]);
-      input2.val(dates[1]);
-    }
-  });
+//   $(this).find(".datepicker-toggle").datepicker({
+//     clearButton: true,
+//     multipleDatesSeparator: ' - ',
+//     range: true,
+//     prevHtml: 'arrow_back',
+//     nextHtml: 'arrow_forward',
+//     navTitles: {
+//       days: 'MM <i>yyyy</i>'
+//   },
+//     onSelect: (date) => {
+//       const dates = date.split(" - ");
+//       input1.val(dates[0]);
+//       input2.val(dates[1]);
+//     }
+//   });
 
-  const datepicker = $(this).find(".datepicker-toggle").data("datepicker");
-  const button = $(`<span class='datepicker--btn'>Применить</span>`);
-  datepicker.$datepicker.find(".datepicker--buttons").append(button);
+//   const datepicker = $(this).find(".datepicker-toggle").data("datepicker");
+//   const button = $(`<span class='datepicker--btn'>Применить</span>`);
+//   datepicker.$datepicker.find(".datepicker--buttons").append(button);
   
-  input1.click(() => datepicker.show());
-  input2.click(() => datepicker.show());
+//   input1.click(() => datepicker.show());
+//   input2.click(() => datepicker.show());
 
-  $(".datepicker--btn").click(() => {
-      if (datepicker.selectedDates.length < 2) return;
-      datepicker.hide();
-    });
-})
+//   $(".datepicker--btn").click(() => {
+//       if (datepicker.selectedDates.length < 2) return;
+//       datepicker.hide();
+//     });
+// })
 
 $(".cards__calendar").each(function () {
   $(this).datepicker({
