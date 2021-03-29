2116f5b2ffa08a3c44345c7d4116c3d4b887e4d9
webproject67 <webproject67@gmail.com>
2021-03-29 09:35:47 +0300

    Добавил проверку ввода email

--- src/pug/blocks/input/input.ts
+++ src/pug/blocks/input/input.ts
@@ -1,4 +1,4 @@
-import {getBool} from '../../../scripts/utils';
+import {getBool, getBoolEmail} from '../../../scripts/utils';
 
 $('#birthdate').each(() => {
   $('#birthdate').on('change', () => {
@@ -10,3 +10,14 @@ $('#birthdate').each(() => {
     }
   });
 });
+
+$('.input__submit').on('click', (evt) => {
+  evt.preventDefault();
+  const value:string = <string>$('#email').val();
+  if(!getBoolEmail(value)) {
+    $('#email').css('border', '1px solid red');
+  } else {
+    $('#email').css('border', '1px solid rgba(31, 32, 65, 0.25)');
+    $('#email').val('Спасибо!');
+  }
+})
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,3 +1,5 @@
 export const getNum = (num:number):string => num.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
 
 export const getBool = (date:string):boolean => /^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/.test(date);
+
+export const getBoolEmail = (date:string):boolean => /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(date);
