81841422bf78303355c3c4ac063d62009ad71904
webproject67 <webproject67@gmail.com>
2021-08-14 20:31:07 +0300

    bem

--- src/pug/blocks/button-rate/button-rate.ts
+++ src/pug/blocks/button-rate/button-rate.ts
@@ -5,8 +5,5 @@ interface JQuery {
 const $buttonRate = $('.js-button-rate');
 $buttonRate.each(function () {
   $(this).addRating({ selectedRatings: $(this).data('count') });
-  $(this)
-    .find('i')
-    .removeClass('material-icons')
-    .addClass('button-rate__icon');
+  $(this).find('i').removeClass('material-icons').addClass('button-rate__icon');
 });
